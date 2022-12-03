d64d0534eb46c44ef4b4a52791fa74a1fad4fbea
gxdxx <nkd0310@naver.com>
2022-12-03 17:33:15 +0900

    #1 - 스프링 부트 프로젝트 생성

---
+++ src/test/java/com/gxdxx/programadmin/ProgramAdminProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxdxx.programadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramAdminProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
