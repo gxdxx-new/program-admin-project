15401650f314cced8e37916c98377d8db18584cb
gxdxx <nkd0310@naver.com>
2022-12-19 02:17:11 +0900

    #17 - 회원 등록 기능 구현

--- src/main/java/com/gxdxx/programadmin/controller/MemberController.java
+++ src/main/java/com/gxdxx/programadmin/controller/MemberController.java
@@ -1,12 +1,17 @@
 package com.gxdxx.programadmin.controller;
 
 import com.gxdxx.programadmin.dto.MemberFormDto;
+import com.gxdxx.programadmin.entity.Member;
 import com.gxdxx.programadmin.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -19,6 +24,24 @@ public class MemberController {
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memberNew(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "members/form";
+        }
+
+        Member member = Member.of(memberFormDto.getMemberId(), memberFormDto.getPassword(), memberFormDto.getEmail(), memberFormDto.getNickname());
+        try {
+            memberService.saveMember(member);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "회원가입 중 에러가 발생했습니다.");
+            return "members/form";
+        }
+
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/gxdxx/programadmin/service/MemberService.java
+++ src/main/java/com/gxdxx/programadmin/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.gxdxx.programadmin.service;
 
+import com.gxdxx.programadmin.entity.Member;
 import com.gxdxx.programadmin.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,5 +15,15 @@ import javax.transaction.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public Long saveMember(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByMemberId(member.getMemberId());
+    }
 
 }
