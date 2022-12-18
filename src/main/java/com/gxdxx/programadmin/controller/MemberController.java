3dd199bc4cb448b416270e4780711a318e3a3bb8
gxdxx <nkd0310@naver.com>
2022-12-19 02:08:40 +0900

    #17 - 회원가입 페이지 이동 구현
    MemberDto 생성

--- src/main/java/com/gxdxx/programadmin/controller/MemberController.java
+++ src/main/java/com/gxdxx/programadmin/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.gxdxx.programadmin.controller;
 
+import com.gxdxx.programadmin.dto.MemberFormDto;
 import com.gxdxx.programadmin.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -11,5 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
 
 }
---
+++ src/main/java/com/gxdxx/programadmin/dto/MemberFormDto.java
@@ -0,0 +1,29 @@
+package com.gxdxx.programadmin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberFormDto {
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 20, message = "아이디는 8자 이상, 20자 이하로 입력해주세요.")
+    private String memberId;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String password;
+
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String email;
+
+    @Length(min = 2, max = 100, message = "닉네임은 2자 이상, 100자 이하로 입력해주세요.")
+    private String nickname;
+
+}
