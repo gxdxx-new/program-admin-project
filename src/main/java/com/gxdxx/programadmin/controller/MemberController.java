9ab90242c120402b6848cf9883c0bbe5b7596163
gxdxx-new <nkd0310@naver.com>
2022-12-27 22:08:14 +0900

    #19 - fix: 회원가입 페이지 html 경로 오타 수정

--- src/main/java/com/gxdxx/programadmin/controller/MemberController.java
+++ src/main/java/com/gxdxx/programadmin/controller/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
     @GetMapping(value = "/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
-        return "member/memberForm";
+        return "members/form";
     }
 
     @PostMapping(value = "/new")
