ec26d4159f2e358381e27fa4744e417396076350
gxdxx-new <nkd0310@naver.com>
2023-02-02 23:38:41 +0900

    #33_ 회원가입_성공 단위테스트 작성

--- src/test/java/com/gxdxx/programadmin/service/MemberServiceTest.java
+++ src/test/java/com/gxdxx/programadmin/service/MemberServiceTest.java
@@ -1,11 +1,24 @@
 package com.gxdxx.programadmin.service;
 
+import com.gxdxx.programadmin.dto.MemberFormDto;
+import com.gxdxx.programadmin.entity.Member;
 import com.gxdxx.programadmin.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -17,5 +30,48 @@ class MemberServiceTest {
     private MemberRepository memberRepository;
     @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("회원가입_성공")
+    public void saveMemberSuccess() {
+        //given
+        MemberFormDto memberFormDto = createMemberFormDto();
+        Member member = createMember(memberFormDto);
+
+        Long fakeMemberId = 1L;
+        ReflectionTestUtils.setField(member, "id", fakeMemberId);
+        ReflectionTestUtils.setField(memberService, "passwordEncoder", passwordEncoder);
+
+        //mocking
+        given(passwordEncoder.encode(anyString())).willReturn("encodedPassword");
+        ReflectionTestUtils.setField(member, "password", passwordEncoder.encode(member.getPassword()));
+        given(memberRepository.save(any())).willReturn(member);
+        given(memberRepository.findById(fakeMemberId)).willReturn(Optional.ofNullable(member));
+
+        //when
+        Long newMemberId = memberService.saveMember(memberFormDto.getMemberName(), memberFormDto.getPassword(), memberFormDto.getEmail(), memberFormDto.getNickname());
+
+        //then
+        Member findMember = memberRepository.findById(newMemberId).get();
+        assertEquals(member.getId(), findMember.getId());
+        assertEquals(member.getMemberName(), findMember.getMemberName());
+        assertEquals(member.getEmail(), findMember.getEmail());
+        assertEquals(member.getNickname(), findMember.getNickname());
+        assertEquals(member.getPassword(), findMember.getPassword());
+    }
+
+
+    private Member createMember(MemberFormDto memberFormDto) {
+        return Member.of(memberFormDto.getMemberName(), memberFormDto.getPassword(), memberFormDto.getEmail(), memberFormDto.getNickname());
+    }
+
+    private MemberFormDto createMemberFormDto() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setMemberName("don");
+        memberFormDto.setPassword("123123123");
+        memberFormDto.setEmail("nkd0310@naver.com");
+        memberFormDto.setNickname("dondon");
+        return memberFormDto;
+    }
 
 }
