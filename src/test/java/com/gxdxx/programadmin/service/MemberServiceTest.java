ec345608d3a1643792951fd20ba795e6dc6aac6b
gxdxx-new <nkd0310@naver.com>
2023-02-02 23:38:07 +0900

    #33 - MemberService 단위테스트 기본설정

---
+++ src/test/java/com/gxdxx/programadmin/service/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package com.gxdxx.programadmin.service;
+
+import com.gxdxx.programadmin.repository.MemberRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+}
