cc416299e41864615829662880e9c08a116ae580
gxdxx-new <nkd0310@naver.com>
2023-02-11 17:38:16 +0900

    #37 - MemberNotFoundException 예외 처리
    memberName에 해당하는 member가 없을 경우 예외 처리

--- src/main/java/com/gxdxx/programadmin/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/gxdxx/programadmin/advice/ControllerExceptionAdvice.java
@@ -49,6 +49,12 @@ public class ControllerExceptionAdvice {
         return "companies/form";
     }
 
+    @ExceptionHandler(MemberNotFoundException.class)
+    public String memberNotFoundException(Model model) {
+        model.addAttribute("errorMessage", "존재하지 않는 회원입니다.");
+        return "index";
+    }
+
     @ExceptionHandler({EventException.class, RuntimeException.class})
     public String eventErrorHandler(Model model){
         return "error";
