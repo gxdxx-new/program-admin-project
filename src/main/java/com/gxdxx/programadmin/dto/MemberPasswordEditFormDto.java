a6ad612b49ff113c875351d9597ffc3daaf2adee
gxdxx-new <nkd0310@naver.com>
2023-02-12 10:44:14 +0900

    #37 - MemberPasswordEditFormDto 생성
    비밀번호 변경 요청을 받기 위한 DTO

---
+++ src/main/java/com/gxdxx/programadmin/dto/MemberPasswordEditFormDto.java
@@ -0,0 +1,25 @@
+package com.gxdxx.programadmin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberPasswordEditFormDto {
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String currentPassword;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String changePassword;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String changePasswordCheck;
+
+}
