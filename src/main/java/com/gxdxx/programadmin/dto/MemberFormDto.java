9c4b2f281138853168227dddc3d96bd89ae9f1f7
gxdxx-new <nkd0310@naver.com>
2023-02-13 18:01:55 +0900

    #37 - MemberFormDto email 필드에 @NotBlank 추가
    @Email 만 붙일 경우 이메일 형식인지는 체크하지만,
    "" 공백을 입력할 경우 정상적인 이메일로 인식하는 문제가 있었다.
    @NotEmpty를 추가해 ""일 경우도 체크하도록 수정했다.

--- src/main/java/com/gxdxx/programadmin/dto/MemberFormDto.java
+++ src/main/java/com/gxdxx/programadmin/dto/MemberFormDto.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 @Getter
@@ -20,6 +19,7 @@ public class MemberFormDto {
     @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotEmpty
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
